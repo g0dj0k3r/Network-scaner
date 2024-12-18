aae1e7dab760587ca2de2439d553d73f3b8a70d2
godj0ker <godjoker2004@gmail.com>
2024-12-18 18:27:56 +0530

    Add files via upload

---
+++ osdiscovery.py
@@ -0,0 +1,38 @@
+import nmap
+from pyfiglet import *
+from colorama import *
+
+scanner=nmap.PortScanner()
+f=Figlet()
+def os_discovery():
+    
+    print(Fore.GREEN+f.renderText("OS DISCOVERY"))
+    
+    target=input("enter target :")
+    scanner.scan(target,arguments='-O')
+
+    for host in scanner.all_hosts():
+        
+        scanner.scan(host,arguments='-O')
+        
+        if 'osmatch' in scanner[host]:
+            for osmatch in scanner[host]['osmatch']:
+                
+                osversion=osmatch.get('osgen', 'Unknown version')
+                osname=osmatch['name']
+                if 'osclass' in osmatch:
+                    
+                    for osclass in osmatch['osclass']:
+                        
+                        
+                        print("ip :",host)
+                        
+                        print("os type :",osclass['type'])
+                        
+                        print("os name:",osname)
+                        
+                        print("os family:",osclass['osfamily'])
+                        
+                        print("VERSION :",osversion)
+                        
+                        print("<---------------------------------------------------->")

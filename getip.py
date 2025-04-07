b0ca6caa0db911c9f643cfb257124cc5111bd0d2
godj0ker <godjoker2004@gmail.com>
2025-04-07 20:58:16 +0530

    Add files via upload

--- getip.py
+++ getip.py
@@ -8,6 +8,14 @@ def get_ip():
     print(Fore.LIGHTRED_EX+f.renderText("GET URL IP"))
     
     target=input("ENTER TARGET URL TO GET IP:")
-    get_ip=socket.gethostbyname(target)
-
-    print(get_ip)
+    
+    if not target:
+        print(Fore.RED+f"Please enter a valid URL"+Style.RESET_ALL)
+        return
+    try:
+        get_ip=socket.gethostbyname(target)
+        print(Fore.GREEN+f"IP OF {target} : {get_ip}"+Style.RESET_ALL)
+    except socket.gaierror:
+        print(Fore.RED+f"Error: Unable to resolve {target}"+Style.RESET_ALL)
+    except Exception as e:
+        print(Fore.RED+f"An error occurred: {e}"+Style.RESET_ALL)
